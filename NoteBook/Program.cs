9308fd51ec8ce585b067937abcc65621ba29d0f4
Alexander Nevgin <alexblackstar86@gmail.com>
2022-10-01 22:20:44 +0300

    Исправлено задание 2 (Телефонная книга на базе словаря).

---
+++ NoteBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoteBook
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -6,94 +6,111 @@ namespace PhoneBook
 {
 	static class Program
 	{
+		/// <summary>
+		/// Adds specified phone number (key) and name (value) to the dictionary d.
+		/// </summary>
+		/// <param name="d">The dictionary to add elements to.</param>
+		/// <param name="data">Tuple containing key and value to add.</param>
+		/// <returns>The dictionary with added element</returns>
+		static Dictionary<string,string> Add(Dictionary<string,string> d, (string,string) data)
+		{
+			string p = data.Item1;
+			string n = data.Item2;
+			
+			d.Add(p, n);
+			return d;
+		}
+
+		/// <summary>
+		/// Handles user input
+		/// </summary>
+		/// <returns>The tuple representing key-value pair to add to dictionary</returns>
+		static (string,string) Input()
+		{
+			Console.WriteLine("Введите номер: ");
+			string p = Console.ReadLine();
+			Console.WriteLine("Введите имя: ");
+			string n = Console.ReadLine();
+
+			(string, string) data = (p, n);
+			return data;
+		}
 		
 		/// <summary>
-		/// Adds a provided phone number into a dictionary
+		/// Searches for entries of specified key in the dictionary.
 		/// </summary>
-		/// <returns>A dictionary with a user-specified phone added in</returns>
-		static Dictionary<string, string> Add()
+		/// <param name="d">The dictionary to search in.</param>
+		/// <param name="p">The key to search entries of</param>
+		/// <returns>if exists, a string representing the key-value pair with specified key; otherwise null.</returns>
+		static string Search(Dictionary<string,string> d, string p)
 		{
-			Dictionary<string, string> phoneBook = new Dictionary<string, string>();
-			while (true)
+			bool isContainsKey = d.ContainsKey(p);
+			if (isContainsKey)
 			{
-				Console.WriteLine("Введите номер телефона: ");
-				string phone = Console.ReadLine();
-				Console.WriteLine("Введите имя: ");
-				string name = Console.ReadLine();
-				phoneBook.Add(phone, name);
-				Console.WriteLine("Вводим далее? (y или Д - да, n или Н - нет)");
-				ConsoleKey key = Console.ReadKey().Key;
-				switch (key)
-				{
-					case ConsoleKey.N:
-						Console.WriteLine();
-						Menu();
-						return phoneBook;
-					case ConsoleKey.Y:
-						Console.WriteLine();
-						Add();
-						break;
-					default:
-						Console.WriteLine("Действия не предусмотрено");
-						break;
-				}
-			}
-		}
-
-		/// <summary>
-		/// Displays a menu into the console
-		/// </summary>
-		static void Menu()
-		{
-			Console.WriteLine("Укажите действие (1 - Ввод данных и добавление в телефонную книгу, 2 - поиск по номеру, 0 - выход): ");
-			string action = Console.ReadLine();
-			switch (action)
-			{
-				case "1":
-					Add();
-					break;
-				case "2":
-					Dictionary<string, string> phoneBook = Add();
-					Search(phoneBook);
-					break;
-				case "0": 
-					
-					break;
-				default:
-					Console.WriteLine("Действия не предусмотрено");
-					break;
-			}
-		}
-
-		/// <summary>
-		/// Searches for a specified number (key) in a dictionary
-		/// </summary>
-		/// <param name="phoneBook">A list to search in</param>
-		static void Search(Dictionary<string,string> phoneBook)
-		{
-			Console.WriteLine("Введите номер телефона для поиска: ");
-			string phone = Console.ReadLine();
-			string name;
-
-			bool ifContainsKey = phoneBook.ContainsKey(phone);
-
-			if (ifContainsKey)
-			{
-				Console.WriteLine(phoneBook[phone]);
+				string res = new KeyValuePair<string, string>(p, d[p]).ToString();
+				return res;
 			}
 			else
 			{
-				Console.WriteLine("Номера нет в книге");
+				return null;
 			}
-
 		}
 
+		/// <summary>
+		/// Outputs a dictionary into the console.
+		/// </summary>
+		/// <param name="d">The dictionary to display.</param>
+		static void Output(Dictionary<string, string> d)
+		{
+			foreach (var e in d)
+			{
+				Console.WriteLine($"{e} ");
+			}
+		}
+		
 		static void Main(string[] args)
 		{
+			// Вариант с пустым словарём.
+			// Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+			
+			// Тестовый словарь.
+			Dictionary<string, string> phoneBook = new Dictionary<string, string>()
+			{
+				{"1235454", "Alex"},
+				{"6546565", "Tony"},
+				{"5677788", "Alex"},
+				{"8766865", "Alice"},
+				{"8908706", "Bob"},
+				{"9004499", "Rand"}
+			};
 			while (true)
 			{
-				Menu();
-				break;
+				Console.WriteLine("Для добавления номера в книгу нажмите 1; для поиска номера нажмите 2; "+
+				                  "для отображения книги нажмите 3. Для выхода нажмите q.");
+				string ev = Console.ReadLine();
+				switch (ev)
+				{
+					case "1":
+						(string, string) data = Input();
+						Add(phoneBook, data);
+						break;
+					case "2":
+						Console.WriteLine("Введите номер для поиска: ");
+						string p = Console.ReadLine();
+						string res = Search(phoneBook, p);
+						if (res == null) Console.WriteLine("Номер не найден.");
+						else Console.WriteLine($"Результат поиска: {res}");
+						break;
+					case "3":
+						Output(phoneBook);
+						break;
+					case "q":
+						Environment.Exit(0);
+						break;
+					default:
+						Console.WriteLine("Действия не предусмотрено.");
+						break;
+				}
 			}
 		}
 	}
